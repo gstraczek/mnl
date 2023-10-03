35884aceb7ff9f59905b1d68e8f6b8fffc1a698a
Grzegorz Strączek <grzegorz.straczek@neti-soft.com>
2023-10-03 20:23:25 +0200

    Add frontend template in next.js

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import Drawer from "@mui/material/Drawer";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import MediaCard from "@/components/MediaCard";
+
+export default function HomePage() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <div>
+        <Alert severity="info" sx={{ mt: 2, mb: 5 }}>
+          <AlertTitle>Hello 👋</AlertTitle>
+          This app uses the Next.js App Router and Material UI v5.
+        </Alert>
+        <Grid container rowSpacing={3} columnSpacing={3}>
+          <Grid xs={6}>
+            <MediaCard
+              heading="CIELAB"
+              text="The CIELAB color space, also referred to as L*a*b*, was intended as a perceptually uniform space, where a given numerical change corresponds to a similar perceived change in color."
+            />
+          </Grid>
+        </Grid>
+      </div>
+    </Box>
+  );
+}
---
+++ frontend/src/styles/theme.tsx
@@ -0,0 +1,15 @@
+// styles/theme.js
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976D2"
+    },
+    secondary: {
+      main: "#F50057"
+    }
+  }
+});
+
+export default theme;
